2c02de493c5969def6bd228a7826e7270a8c322f
BOTxJOHN <rouzin.evgeny@gmail.com>
2025-04-20 11:08:47 -0700

    exported deck class

--- js/Deck.js
+++ js/Deck.js
@@ -1,6 +1,6 @@
 import Card from "./Card.js";
 
-class Deck {
+export default class Deck {
     #deck = [];
     #suits = ["♥", "♠", "♣", "♦"];
     #ranks = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
