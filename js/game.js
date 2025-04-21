723f95acc4fa7d21ee49fa8479d145aca9409f08
Vivian Yu <146399020+v3yu@users.noreply.github.com>
2025-04-21 01:46:15 +0000

    added event listeners to game.js for button presses

--- js/game.js
+++ js/game.js
@@ -1,1 +1,24 @@
+import Card from 'js/card.js';
+import Deck from 'js/js';
+import GameManager from 'js/GameManager.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+    const deck = new Deck();
+    const blackjackGame = new GameManager(deck);
+
+    const dealButton = document.getElementById('deal-button');
+    const hitButton = document.getElementById('hit-button');
+    const standButton = document.getElementById('stand-button');
+
+    dealButton.addEventListener('click', () => {
+        blackjackGame.startGame();
+    });
+
+    hitButton.addEventListener('click', () => {
+        blackjackGame.playerHit();
+    });
+
+    standButton.addEventListener('click', () => {
+        blackjackGame.playerStand();
+    });
+});
